daa11caf59b0d741b990c75bac479ec55f979398
J0113 <keskitalo_6@hotmail.com>
2024-12-06 15:47:03 +0100

    Setup simple test example

---
+++ strajk-bowling/src/__tests__/App.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { test, expect } from 'vitest';
+import App from '../App';
+
+test('renders the Strajk Bowling logo', () => {
+    render(<App />);
+    const images = screen.getAllByRole('img');
+    const logo = images.find(img => img.classList.contains('top__logo'));
+    expect(logo).toBeInTheDocument();
+  });
+  
