1ea8b1fc3e6435da67fcee87e2d42eac15b0df0c
J0113boy <keskitalo_6@hotmail.com>
2025-01-03 19:13:05 +0100

    Setup github actions for CI with test coverage validation

--- strajk-bowling/src/setupTests.js
+++ strajk-bowling/src/setupTests.js
@@ -1,13 +1,10 @@
-const fetch = require('node-fetch');  
-global.fetch = fetch;  
-global.Response = fetch.Response;  
-global.Request = fetch.Request;  
-global.Headers = fetch.Headers;  
+import { server } from './mocks/server'; 
 
-import { server } from './mocks/server';  
-
+// Starta servern innan tester
 beforeAll(() => server.listen());
 
+// Återställ MSW handlers efter varje test
 afterEach(() => server.resetHandlers());
 
+// Stäng ner servern efter alla tester
 afterAll(() => server.close());
