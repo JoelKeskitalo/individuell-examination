f6c42cbfe8504e83bb704edb21f1ba24e341e742
J0113boy <keskitalo_6@hotmail.com>
2025-01-03 19:01:50 +0100

    Fix: Resolve Response is not Defined error in tests

--- strajk-bowling/src/setupTests.js
+++ strajk-bowling/src/setupTests.js
@@ -1,3 +1,12 @@
+import('node-fetch').then((fetchModule) => {
+    global.fetch = fetchModule.default;
+    global.Response = fetchModule.Response;
+    global.Request = fetchModule.Request;
+    global.Headers = fetchModule.Headers;
+  });
+  
+
+
 import { server } from './mocks/server';  // Importera mock-servern
 
 beforeAll(() => server.listen());  // Starta servern innan tester
