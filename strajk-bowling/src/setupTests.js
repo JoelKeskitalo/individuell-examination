fd0a318d2d6fcdc6ce77b53415c5757a77545f87
J0113boy <keskitalo_6@hotmail.com>
2025-01-03 19:16:15 +0100

    Setup github actions for CI with test coverage validation

--- strajk-bowling/src/setupTests.js
+++ strajk-bowling/src/setupTests.js
@@ -1,4 +1,11 @@
-import { server } from './mocks/server'; 
+const { fetch, Response, Request, Headers } = require('undici');
+
+global.fetch = fetch;  // Polyfill för fetch
+global.Response = Response;  // Polyfill för Response
+global.Request = Request;  // Polyfill för Request
+global.Headers = Headers;  // Polyfill för Headers
+
+import { server } from './mocks/server';  // Importera mock-servern från din MSW-konfiguration
 
 // Starta servern innan tester
 beforeAll(() => server.listen());
