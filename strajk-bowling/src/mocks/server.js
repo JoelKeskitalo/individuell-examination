daa420485308a390dfbeb31f0dfd17736e617a5d
J0113boy <keskitalo_6@hotmail.com>
2025-01-03 09:23:40 +0100

    Configure server.js

--- strajk-bowling/src/mocks/server.js
+++ strajk-bowling/src/mocks/server.js
@@ -1,4 +1,13 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
+import { setupServer } from 'msw/node';
+import { handlers } from './handlers';
 
 export const server = setupServer(...handlers);
+
+// Startar servern före tester
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+
+// Återställer handlers efter varje test
+afterEach(() => server.resetHandlers());
+
+// Stänger servern efter tester
+afterAll(() => server.close());
