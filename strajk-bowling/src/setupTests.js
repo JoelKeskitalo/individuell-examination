7be9ac80027d36a7b58e8f10e1b369c369c84604
J0113boy <keskitalo_6@hotmail.com>
2025-01-03 19:09:11 +0100

    Fix global fetch

--- strajk-bowling/src/setupTests.js
+++ strajk-bowling/src/setupTests.js
@@ -1,12 +1,13 @@
-const fetch = require('node-fetch'); 
-global.fetch = fetch; 
+const fetch = require('node-fetch');  
+global.fetch = fetch;  
 global.Response = fetch.Response;  
 global.Request = fetch.Request;  
-global.Headers = fetch.Headers; 
-
+global.Headers = fetch.Headers;  
 
 import { server } from './mocks/server';  
 
-beforeAll(() => server.listen());  
-afterEach(() => server.resetHandlers()); 
-afterAll(() => server.close());  
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
