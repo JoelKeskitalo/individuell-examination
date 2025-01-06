74abe3581e537f968b2d879968d4da1989cf4cc3
J0113boy <keskitalo_6@hotmail.com>
2025-01-06 13:06:26 +0100

    Fixed Mock bug by adding additional request in handlers.js

--- strajk-bowling/src/mocks/handlers.js
+++ strajk-bowling/src/mocks/handlers.js
@@ -4,7 +4,7 @@ export const handlers = [
     http.post(
         'https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com',
         async (request) => {
-            const body = await request.json();
+            const body = await request.request.json()
             const { when, people, lanes, shoes } = body;
 
             const price = parseInt(lanes) * 100 + parseInt(people) * 120;
